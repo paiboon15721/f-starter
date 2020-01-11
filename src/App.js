c85c85d130502e50d35989190d1fff3de4915d9c
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2020-01-11 19:38:15 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import Layout from 'components/Layout'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
 import CommentApp from 'pages/CommentApp'
 import Dashboard from 'pages/Dashboard'
 import TestContext from 'pages/TestContext'
@@ -23,6 +28,9 @@ const App = () => {
               <v.component />
             </Route>
           ))}
+          <Route path="/">
+            <Redirect to="/dashboard" />
+          </Route>
         </Switch>
       </Layout>
     </Router>
