da7324dd852ecfdb52c4d036dd18f9d7a2fbb95d
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2020-01-11 16:47:33 +0700

    update

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,9 +2,7 @@ import React from "react";
 
 export default () => (
   <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-    <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">
-      Company name
-    </a>
+    <span className="navbar-brand col-sm-3 col-md-2 mr-0">Company name</span>
     <input
       className="form-control form-control-dark w-100"
       type="text"
@@ -13,9 +11,7 @@ export default () => (
     />
     <ul className="navbar-nav px-3">
       <li className="nav-item text-nowrap">
-        <a className="nav-link" href="#">
-          Sign out
-        </a>
+        <span className="nav-link">Sign out</span>
       </li>
     </ul>
   </nav>
