5c36a9d43b542f69e8d4742e81b3a200fae0ae79
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2020-01-11 19:02:18 +0700

    update

--- src/pages/TestContext.js
+++ src/pages/TestContext.js
@@ -1,5 +1,48 @@
-import React from "react";
+import React, { useState, createContext, useContext } from "react";
 
-const App = () => <h1>hello</h1>;
+const globalTheme = ["danger", "primary", "success", "warning"];
+
+const themeContext = createContext();
+
+const Header = () => (
+  <themeContext.Consumer>
+    {v => (
+      <div className={`alert alert-${v.theme}`} role="alert">
+        Comment App!
+      </div>
+    )}
+  </themeContext.Consumer>
+);
+
+const ChangeTheme = () => {
+  const v = useContext(themeContext);
+
+  return (
+    <>
+      <button className={`btn btn-${v.theme}`} onClick={v.changeTheme}>
+        Change Theme
+      </button>
+      <Header />
+      <Header />
+      <Header />
+    </>
+  );
+};
+
+const App = () => {
+  const [theme, setTheme] = useState("primary");
+
+  const changeTheme = () => {
+    setTheme(globalTheme[Math.floor(Math.random() * globalTheme.length)]);
+  };
+
+  return (
+    <themeContext.Provider value={{ theme, changeTheme }}>
+      <ChangeTheme />
+      <h1>hello</h1>
+      <Header />
+    </themeContext.Provider>
+  );
+};
 
 export default App;
